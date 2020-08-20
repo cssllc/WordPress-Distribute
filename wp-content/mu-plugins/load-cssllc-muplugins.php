0f29e83aecc73947e7dfc8a13e7bf0d101904aaf
Caleb Stauffer <develop+github@calebstauffer.com>
2020-08-20 10:52:52 -0400

    minor adjustments to CSSLLC mu-plugins loader

--- wp-content/mu-plugins/load-cssllc-muplugins.php
+++ wp-content/mu-plugins/load-cssllc-muplugins.php
@@ -2,7 +2,7 @@
 /**
  * Include all CSSLLC mu-plugins.
  *
- * Description: Collection of WordPress mu-plugins to enhance and customize a WordPress installation.
+ * Description: Collection of WordPress mu-plugins to enhance, customize, and protect a WordPress installation.
  * Plugin URI: https://github.com/cssllc/mu-plugins
  * Author: Caleb Stauffer
  */
@@ -33,9 +33,13 @@ function load_cssllc_muplugins() {
 
 	foreach ( $files as $file ) {
 		require_once 'cssllc/' . $file;
-		$loaded_files[] = $file;
+		$loaded_files[] = '├─ ' . $file;
 		do_action( 'mu_plugin_loaded', 'cssllc/' . $file );
 	}
+
+	$last_file = array_pop( $loaded_files );
+	$last_file = str_replace( '├', '└', $last_file );
+	$loaded_files[] = $last_file;
 
 	do_action( 'cssllc_muplugins_loaded' );
 
@@ -43,7 +47,7 @@ function load_cssllc_muplugins() {
 		if ( basename( __FILE__ ) !== $file )
 			return $actions;
 
-		array_push( $actions, '<span style="color: #999;">' . implode( '<br />', $loaded_files ) ) . '</span>';
+		array_push( $actions, '<span style="display: inline-block; font-family: Consolas, Monaco, monospace; font-size: 0.9em; line-height: 1.2em; color: #333;">' . implode( '<br />', $loaded_files ) ) . '</span>';
 
 		return $actions;
 	}, 9999, 2 );
